e2e4c404529677add5800c4283408374704e8b7c
CadenWhaley <60452014+CadenWhaley@users.noreply.github.com>
2021-10-19 12:56:04 -0400

    Update config.example.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,15 +1,15 @@
 const config = {
-	ownerID: ['YourAccountID'],
-	token: 'YourBotToken',
+	ownerID: ['878096206587363418'],
+	token: 'ODk5NDI0NzA4Mjg0NTI2NTky.YWykiA.T83_lbwb0P46yLKV-JhMrwfBYCY',
 	// For looking up Twitch, Fortnite, Steam accounts
 	api_keys: {
 		// https://dev.twitch.tv/console/apps
 		twitch: {
-			clientID: '',
-			clientSecret: '',
+			clientID: '7z0dgvs0v86llvwuvafxu1jggly84c',
+			clientSecret: '84apily8rmaqoyw7gqupksxix36yoo',
 		},
 		// https://fortnitetracker.com/site-api
-		fortnite: 'fortniteAPI-Key',
+		fortnite: 'a16b858670e09bc379f71081a1c92d55226d59f8',
 		// https://steamcommunity.com/dev
 		steam: 'steamAPI-Key',
 		// https://developer.spotify.com/documentation/web-api/
@@ -23,9 +23,9 @@ const config = {
 			password: 'password',
 		},
 		// https://genius.com/developers
-		genuis: 'genuisAPI-KEY',
+		genuis: 'ICq7HxRAKBfsPNkIA50N4qLHwG8LF23uKiKOJ1g1FRTnSJOi8swvbrvBCw0kA-cY',
 		// https://api.amethyste.moe/
-		amethyste: 'amethysteAPI-Key',
+		amethyste: 'c6a339fe73ec2f59f932410e3aa46982f1148aeabb065640ef1859c5faa63ae20cbb366e91408536632b5e288339269ad60ba97c88f59cd6bf53e6558e9570dd',
 	},
 	// add plugins/commands here if you don't want them loaded in the bot.
 	disabledCommands: [],
@@ -45,7 +45,7 @@ const config = {
 		GuildChannel: '761619652009787392',
 	},
 	// URL to mongodb
-	MongoDBURl: 'mongodb://link',
+	MongoDBURl: 'mongodb+srv://tc3NXXu7mzhk4lJq:CJsHWWUMjWjePD9S@cluster0.chlqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 	// embed colour
 	embedColor: 'RANDOM',
 	// This will spam your console if you enable this but will help with bug fixing
